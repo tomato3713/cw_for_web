de8e0ec1863064f0b1cbe3d446c7748107df1ee6
Taichi <weasel.wt@gmail.com>
2017-06-21 18:55:48 +0900

    renew the UI

--- cw_runner.js
+++ cw_runner.js
@@ -4,48 +4,46 @@
 //i :playOn-0, playOff 1;
 // duration_time: time 1, 3, 7
 
-  // Web Audio APIが使えるか確認しつつ、contextをつくる
-  var wpm=25;
-  var SupportedAudioContext;
-  try {
-    SupportedAudioContext = window.AudioContext || window.webkitAudioContext;
-  } catch (e) {
-    throw new Error('Web Audio API is not supported.');
-  }
-  var context = new SupportedAudioContext();
+// Web Audio APIが使えるか確認しつつ、contextをつくる
+var wpm = 25;
+var SupportedAudioContext;
+try {
+  SupportedAudioContext = window.AudioContext || window.webkitAudioContext;
+} catch (e) {
+  throw new Error('Web Audio API is not supported.');
+}
+var context = new SupportedAudioContext();
 
-  // オシレーターをつくる
-  var oscNode = context.createOscillator();
-  oscNode.frequency.value = 630;
-  oscNode.type = 'sine';
+// オシレーターをつくる
+var oscNode = context.createOscillator();
+oscNode.frequency.value = 630;
+oscNode.type = 'sine';
 
-  // 音量調節用のnodeを作成
-  var gainNode = context.createGain();
+// 音量調節用のnodeを作成
+var gainNode = context.createGain();
+gainNode.gain.value = 0;
+
+// もろもろつなげる
+oscNode.connect(gainNode);
+gainNode.connect(context.destination);
+
+oscNode.start(0);
+
+function signalOn(duration_time) {
+  // play the shorter signal
+  gainNode.gain.value = 10;
+  //play beep sound time
+  var start_Time = new Date();
+  do {
+    var end_Time = new Date();
+  } while ((end_Time - start_Time) < (wpm / 25) * 30 * duration_time)
   gainNode.gain.value = 0;
-
-  // もろもろつなげる
-  oscNode.connect(gainNode);
-  gainNode.connect(context.destination);
-
-  oscNode.start(0);
-
-function Play(i, duration_time) {
-  if (i == 0) {
-    // play the shorter signal
-    gainNode.gain.value=10;
-    //play beep sound time
-    var start_Time = new Date();
-    do{
-      var end_Time = new Date();
-    } while ((end_Time - start_Time) < (wpm /25) *30 *duration_time)
-      gainNode.gain.value=0;
-  }
-  if (i == 1){
-    gainNode.gain.value=0;
-    //stop beep sound time
-    var start_Time = new Date();
-    do{
-      var end_Time = new Date();
-    } while ((end_Time - start_Time) < (wpm / 25) * 50 * duration_time)
-  }
+}
+function signalOff(duration_time) {
+  gainNode.gain.value = 0;
+  //stop beep sound time
+  var start_Time = new Date();
+  do {
+    var end_Time = new Date();
+  } while ((end_Time - start_Time) < (wpm / 25) * 50 * duration_time)
 }
--- select_signal.js
+++ select_signal.js
@@ -1,219 +1,229 @@
 //call the function play()
 //by call_name which registered alphabet and number
 var call_name = new Array;
-var call_answer;
+var call_answer = new Array;
 var turn = 1;
-function select_signal_play() {
+function select_signal() {
     var row = Math.floor(Math.random() * (call_name.length));
-    call_answer = call_name[row];
     for (var column = 0; call_name[row][column] != 'END'; column++) {
         //alphabet
         if (call_name[row][column] == 'A') {
-            Play(0, 1); Play(1, 1);//play short signal and stop
-            Play(0, 3);//play long signal
+            signalOn(1); signalOff(1);//play short signal and stop
+            signalOn(3);//play long signal
         }
         if (call_name[row][column] == 'B') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'C') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 1);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'D') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'E') {
-            Play(0, 1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'F') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1);
+            signalOn(1); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'G') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 1);
+            signalOn(3); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'H') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'I') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 1);
+            signalOn(1); signalOff(1, 1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'J') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 3);
+            signalOn(1); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(3);
         }
         if (call_name[row][column] == 'K') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 3);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(3);
         }
         if (call_name[row][column] == 'L') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 1);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'M') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 3);
+            signalOn(3); signalOff(1);
+            signalOn(3);
         }
         if (call_name[row][column] == 'N') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 1);
+            signalOn(3); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'O') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 3); Play(1, 1);
+            signalOn(3); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(3);
 
         }
         if (call_name[row][column] == 'P') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 1);
+            signalOn(1); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'Q') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 3);
+            signalOn(3); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(3);
         }
         if (call_name[row][column] == 'R') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 1);
+            signalOn(1); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'S') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1);
         }
         if (call_name[row][column] == 'T') {
-            Play(0, 3);
+            signalOn(3);
         }
         if (call_name[row][column] == 'U') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 3);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(3);
         }
         if (call_name[row][column] == 'V') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 3);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(3);
         }
         if (call_name[row][column] == 'W') {
-            Play(0, 1); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 3);
+            signalOn(1); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(3);
         }
         if (call_name[row][column] == 'X') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 3);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(3);
         }
         if (call_name[row][column] == 'Y') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 3);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(3);
         }
         if (call_name[row][column] == 'Z') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1);
+            signalOn(3); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1);
         }
         //end alpahbet
 
         //Number
         if (call_name[row][column] == 0) {
             for (var s = 1; s <= 4; s++) {
-                Play(0, 3); Play(1, 1);
+                signalOn(3); signalOffal(1);
             }
-            Play(0, 3);
+            signalOn(3);
         }
         if (call_name[row][column] >= 1 && call_name[row][column] <= 4) {
             for (var s = 1; s <= call_name[row][column]; s++) {
-                Play(0, 1); Play(1, 1);
+                signalOn(1); signalOff(1, 1);
             }
             for (var l = 1; l <= (5 - call_name[row][column]); l++) {
-                Play(0, 3); Play(1, 1);
+                signalOn(3); signalOff(1, 1);
             }
-            Play(0, 3);
+            signalOn(3);
         }
         if (call_name[row][column] == 5) {
             for (var s = 1; s <= 4; s++) {
-                Play(0, 1); Play(1, 1);
+                signalOn(1); signalOff(1);
             }
-            Play(0, 1);
+            signalOn(1);
         }
 
         if (call_name[row][column] >= 6 && call_name[row][column] <= 9) {
             for (var s = 1; s <= (call_name[row][column] - 5); s++) {
-                Play(0, 3); Play(1, 1);
+                signalOn(3); signalOff(1);
             }
             for (var l = 1; l <= (5 - (call_name[row][column] - 5) - 1); l++) {
-                Play(0, 1); Play(1, 1);
+                signalOn(1); signalOff(1);
             }
-            Play(0, 1);
+            signalOn(1);
         }
         if (call_name[row][column] == '/') {
-            Play(0, 3); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 1); Play(1, 1);
-            Play(0, 3); Play(1, 1);
-            Play(0, 1);
+            signalOn(3); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(1); signalOff(1);
+            signalOn(3); signalOff(1);
+            signalOn(1);
         }
-        Play(1, 3); //stop signal
+        signalOff(3); //stop signal
     }
     turn = 0;
+    for (var i = 0; i <= call_name[row].length - 2; i++) {
+        call_answer[i] = call_name[row][i];
+    }
 }
 
 function answerCheck() {
     if (turn == 0) {
         var myAnswer = document.getElementById("Box").value.split('');
-        myAnswer.push('END');
 
         var result = 0;
         var result_dif = new Array();
         for (i = 0; i <= call_answer.length - 1 && i <= myAnswer.length - 1; i++) {
             if (call_answer[i] == myAnswer[i]) {
-                result_dif[i] = '0';
+                result_dif[i] = 'R';
             } else {
-                result_dif[i] = '1';
+                result_dif[i] = 'W';
                 result++;
             }
         }
 
-        var Num = parseInt(document.getElementById("Result_Num").value);
+        var right_counter = parseInt(document.getElementById("RightCount").value);
+        var wrong_counter = parseInt(document.getElementById("WrongCount").value);
         if (result == 0) {
-            document.getElementById("Result_Now").value = 'R';
-            document.getElementById("Result_Num").value = Num + 1;
+            document.getElementById("Result_Now").value = 'R_' + result_dif.join(',');
+            document.getElementById("RightCount").value = right_counter + 1;
         }
         if (result != 0) {
             document.getElementById("Result_Now").value = 'W_' + result_dif.join(',');
+            document.getElementById("WrongCount").value = wrong_counter + 1;
         }
         document.getElementById('Box').value = '';
         turn = 1;
+
+        var history = new String();
+        history = document.getElementById('History').value;
+            history += '\n';
+        history += call_answer.join('') + '-' + myAnswer.join('');
+        document.getElementById('History').value = history;
+
     }
 }
