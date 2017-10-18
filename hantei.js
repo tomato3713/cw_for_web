6cc6e2561e08f16b20ea6ca0f545d8f6703eda60
Taichi <weasel.wt@gmail.com>
2017-10-18 12:33:55 +0900

    completed

--- hantei.js
+++ hantei.js
@@ -1,52 +1,55 @@
-window.onload(){
-  var domCalsign = document.getElementById('id_box')
-  var domCwPoint = document.getElementById('id_cw_point')
-  var domPhPoint = document.getElementById('id_ph_point')
+window.onload = function(){
+  var domCalsign = document.getElementById('id_box');
+  var domCwPoint = document.getElementById('id_cw_point');
 
-  var CallSignChar = domCalsign.value.split("")
+  function hanteiEvent(){
+  var CallSignChar = domCalsign.value.split("");
+    var changedCWCallSign = new Array;
+
+
   for( var i = 0; i <= CallSignChar.length; i++){
-    if( CallSignChar[i] == "A") changedCWCallSign[i] == (".-").split("");
-    if( CallSignChar[i] == "B") changedCWCallSign[i] == ("-...").split("");
-    if( CallSignChar[i] == "C") changedCWCallSign[i] == ("-.-.").split("");
-    if( CallSignChar[i] == "D") changedCWCallSign[i] == ("-..").split(""); 
-    if( CallSignChar[i] == "E") changedCWCallSign[i] == (".").split("");
-    if( CallSignChar[i] == "F") changedCWCallSign[i] == ("..-.").split("");
-    if( CallSignChar[i] == "G") changedCWCallSign[i] == ("--.").split(""); 
-    if( CallSignChar[i] == "H") changedCWCallSign[i] == ("....").split("");
-    if( CallSignChar[i] == "I") changedCWCallSign[i] == ("..").split("");
-    if( CallSignChar[i] == "J") changedCWCallSign[i] == (".---").split("");
-    if( CallSignChar[i] == "K") changedCWCallSign[i] == ("-.-").split(""); 
-    if( CallSignChar[i] == "L") changedCWCallSign[i] == (".-..").split("");
-    if( CallSignChar[i] == "M") changedCWCallSign[i] == ("--").split("");
-    if( CallSignChar[i] == "N") changedCWCallSign[i] == ("-.").split("");
-    if( CallSignChar[i] == "O") changedCWCallSign[i] == ("---").split(""); 
-    if( CallSignChar[i] == "P") changedCWCallSign[i] == (".--.").split("");
-    if( CallSignChar[i] == "Q") changedCWCallSign[i] == ("--.-").split("");
-    if( CallSignChar[i] == "R") changedCWCallSign[i] == (".-.").split(""); 
-    if( CallSignChar[i] == "S") changedCWCallSign[i] == ("...").split(""); 
-    if( CallSignChar[i] == "T") changedCWCallSign[i] == ("-").split("");
-    if( CallSignChar[i] == "U") changedCWCallSign[i] == ("..-").split(""); 
-    if( CallSignChar[i] == "V") changedCWCallSign[i] == ("...-").split("");
-    if( CallSignChar[i] == "W") changedCWCallSign[i] == (".--").split(""); 
-    if( CallSignChar[i] == "X") changedCWCallSign[i] == ("-..-").split("");
-    if( CallSignChar[i] == "Y") changedCWCallSign[i] == ("-.--").split("");
-    if( CallSignChar[i] == "Z") changedCWCallSign[i] == ("--..").split("");
-    if( CallSignChar[i] == "1") changedCWCallSign[i] == (".----").split("");
-    if( CallSignChar[i] == "2") changedCWCallSign[i] == ("..---").split("");
-    if( CallSignChar[i] == "3") changedCWCallSign[i] == ("...--").split("");
-    if( CallSignChar[i] == "4") changedCWCallSign[i] == ("....-").split("");
-    if( CallSignChar[i] == "5") changedCWCallSign[i] == (".....").split("");
-    if( CallSignChar[i] == "6") changedCWCallSign[i] == ("-....").split("");
-    if( CallSignChar[i] == "7") changedCWCallSign[i] == ("--...").split("");
-    if( CallSignChar[i] == "8") changedCWCallSign[i] == ("---..").split("");
-    if( CallSignChar[i] == "9") changedCWCallSign[i] == ("----.").split("");
-    if( CallSignChar[i] == "0") changedCWCallSign[i] == ("-----").split("");
+    if( CallSignChar[i] == "A") changedCWCallSign[i] = (".-").split("");
+    if( CallSignChar[i] == "B") changedCWCallSign[i] = ("-...").split("");
+    if( CallSignChar[i] == "C") changedCWCallSign[i] = ("-.-.").split("");
+    if( CallSignChar[i] == "D") changedCWCallSign[i] = ("-..").split(""); 
+    if( CallSignChar[i] == "E") changedCWCallSign[i] = (".").split("");
+    if( CallSignChar[i] == "F") changedCWCallSign[i] = ("..-.").split("");
+    if( CallSignChar[i] == "G") changedCWCallSign[i] = ("--.").split(""); 
+    if( CallSignChar[i] == "H") changedCWCallSign[i] = ("....").split("");
+    if( CallSignChar[i] == "I") changedCWCallSign[i] = ("..").split("");
+    if( CallSignChar[i] == "J") changedCWCallSign[i] = (".---").split("");
+    if( CallSignChar[i] == "K") changedCWCallSign[i] = ("-.-").split(""); 
+    if( CallSignChar[i] == "L") changedCWCallSign[i] = (".-..").split("");
+    if( CallSignChar[i] == "M") changedCWCallSign[i] = ("--").split("");
+    if( CallSignChar[i] == "N") changedCWCallSign[i] = ("-.").split("");
+    if( CallSignChar[i] == "O") changedCWCallSign[i] = ("---").split(""); 
+    if( CallSignChar[i] == "P") changedCWCallSign[i] = (".--.").split("");
+    if( CallSignChar[i] == "Q") changedCWCallSign[i] = ("--.-").split("");
+    if( CallSignChar[i] == "R") changedCWCallSign[i] = (".-.").split(""); 
+    if( CallSignChar[i] == "S") changedCWCallSign[i] = ("...").split(""); 
+    if( CallSignChar[i] == "T") changedCWCallSign[i] = ("-").split("");
+    if( CallSignChar[i] == "U") changedCWCallSign[i] = ("..-").split(""); 
+    if( CallSignChar[i] == "V") changedCWCallSign[i] = ("...-").split("");
+    if( CallSignChar[i] == "W") changedCWCallSign[i] = (".--").split(""); 
+    if( CallSignChar[i] == "X") changedCWCallSign[i] = ("-..-").split("");
+    if( CallSignChar[i] == "Y") changedCWCallSign[i] = ("-.--").split("");
+    if( CallSignChar[i] == "Z") changedCWCallSign[i] = ("--..").split("");
+    if( CallSignChar[i] == "1") changedCWCallSign[i] = (".----").split("");
+    if( CallSignChar[i] == "2") changedCWCallSign[i] = ("..---").split("");
+    if( CallSignChar[i] == "3") changedCWCallSign[i] = ("...--").split("");
+    if( CallSignChar[i] == "4") changedCWCallSign[i] = ("....-").split("");
+    if( CallSignChar[i] == "5") changedCWCallSign[i] = (".....").split("");
+    if( CallSignChar[i] == "6") changedCWCallSign[i] = ("-....").split("");
+    if( CallSignChar[i] == "7") changedCWCallSign[i] = ("--...").split("");
+    if( CallSignChar[i] == "8") changedCWCallSign[i] = ("---..").split("");
+    if( CallSignChar[i] == "9") changedCWCallSign[i] = ("----.").split("");
+    if( CallSignChar[i] == "0") changedCWCallSign[i] = ("-----").split("");
   }
 
-"二次元配列を一次元に直す。
+//二次元配列を一次元に直す。
   var changedCWCallSign = Array.prototype.concat.apply([], changedCWCallSign);
 
-  ".の個数を数えてNum_dotに個数を代入する
+  //.の個数を数えてNum_dotに個数を代入する
   var Num_dot = 0;
   i = 0;
   while( i <= changedCWCallSign.length -1 ){
@@ -58,5 +61,11 @@ window.onload(){
     }
   }
 
-  "頂点の個数 - 短点の個数
-  domCwPoint = (changedCWCallSign.length - Num_dot ) * 1.5 Num_dot * (-0.7);
+  // 30 *  短点の個数 / 頂点の個数
+  domCwPoint.value = 30 * ( (changedCWCallSign.length - Num_dot) / Num_dot );
+  }
+
+  //add event hantei
+  document.getElementById('id_button').addEventListener('click', hanteiEvent, false);
+
+}
