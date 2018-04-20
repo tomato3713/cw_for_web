2ca25a4ba18f25eb8ee7779eea84fa88dfd437ff
homedm <weasel.wt@gmail.com>
2018-04-20 12:37:10 +0900

    fix

--- common.js
+++ common.js
@@ -7,7 +7,7 @@ document.addEventListener('click', () => {
 	gainNode.gain.setValueAtTime(0, context.currentTime);
 	node.connect(context.destination);
 	node.start();
-	node.stop( context.currentTime );
+	node.stop( context.currentTime+1 );
 	document.removeEventListener('click', this);
 });
 
