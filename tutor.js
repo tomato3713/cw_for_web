61638717187c76e1981cc6483c3803f77cf10186
tomato3713 <weasel.wt@gmail.com>
2020-03-06 11:30:03 +0900

    Format by lsp server

--- tutor.js
+++ tutor.js
@@ -1,25 +1,25 @@
 "use strict";
 //textBox, id:Boxに書かれている文字列のモールス符号を再生する
 const play_all = () => {
-	if ( document.getElementById("Box").value !=  '' ) {
-		const call = document.getElementById("Box").value.toUpperCase().split('');
-		const freq = document.getElementById('Freq').value;
-		const wpm = document.getElementById("Speed").value;
-		playMorseNode(call, freq, wpm, context.currentTime, context);
-	}
+    if (document.getElementById("Box").value != '') {
+        const call = document.getElementById("Box").value.toUpperCase().split('');
+        const freq = document.getElementById('Freq').value;
+        const wpm = document.getElementById("Speed").value;
+        playMorseNode(call, freq, wpm, context.currentTime, context);
+    }
 }
 //delとナンバーアルファベットキーのどちらが入力されたのか判断する。
 const keyboardButtonCheck = (e) => {
     const target = e.target;
-    if(target.type === 'button' && target.value != ""){
-      if (target.value === "del") {
-          ClickOnDel();
-      } else {
-		  ClickOn(target.value);
-		  const freq = document.getElementById('Freq').value;
-		  const wpm = document.getElementById("Speed").value;
-		  playMorseNode(target.value, freq, wpm, context.currentTime, context);
-      }
+    if (target.type === 'button' && target.value != "") {
+        if (target.value === "del") {
+            ClickOnDel();
+        } else {
+            ClickOn(target.value);
+            const freq = document.getElementById('Freq').value;
+            const wpm = document.getElementById("Speed").value;
+            playMorseNode(target.value, freq, wpm, context.currentTime, context);
+        }
     }
 }
 
@@ -28,7 +28,7 @@ const keyboardButtonCheck = (e) => {
 const ClickOn = (key) => {
     let Enter_Call = new Array(document.getElementById("Box").value);
     //add the char clicked
-    if ( Enter_Call[0] === '' ){
+    if (Enter_Call[0] === '') {
         //not existed first char.
         Enter_Call[0] = key;
     } else {
@@ -49,20 +49,20 @@ const ClickOnDel = () => {
 
 // if you use pc, register to event listener.
 const keyDown = (e) => {
-	// get key code
-	const keyCode = e.keyCode;
-	if ( keyCode === 13 ) { // Enter key
-		document.getElementById('PlayButton').click();
-	}
-	if( keyCode === 27 ) { // ESC key
-		stopMorse(context.currentTime);
-	}
+    // get key code
+    const keyCode = e.keyCode;
+    if (keyCode === 13) { // Enter key
+        document.getElementById('PlayButton').click();
+    }
+    if (keyCode === 27) { // ESC key
+        stopMorse(context.currentTime);
+    }
 }
 
 //イベントリスナーに関数を登録する。
 const initAddEvent = () => {
-	document.getElementById('PlayButton').addEventListener('click', play_all , false);
-	// document.getElementById('key').addEventListener('click', keyboardButtonCheck, false);
+    document.getElementById('PlayButton').addEventListener('click', play_all, false);
+    // document.getElementById('key').addEventListener('click', keyboardButtonCheck, false);
 
     // define key command
     document.addEventListener('keydown', keyDown);
@@ -72,7 +72,7 @@ try {
     window.AudioContext =
         window.AudioContext || window.webkitAudioContext;
 }
-catch(e) {
+catch (e) {
     alert('Web Audio API is not supported in this browser');
 }
 let context = new AudioContext();
